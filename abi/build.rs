d24fd6588541a1c9bcc72ca99ae8014b34f2e9df
lshoo <lshoo36@gmail.com>
2022-12-09 21:29:42 +0800

    Improve: Add abi proto

---
+++ abi/build.rs
@@ -0,0 +1,10 @@
+fn main() {
+    tonic_build::configure()
+        .out_dir("src/pb")
+        .compile(&["protos/reservation.proto"], &["protos"])
+        .unwrap();
+
+    // fs::remove_file("src/pb/google.protobuf.rs").unwarp();
+
+    println!("cargo:rerun-if-changed=protos/reservation.proto");
+}
