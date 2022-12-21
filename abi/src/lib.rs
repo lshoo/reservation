e4b24dddab02c735ad0ec97574b549dd434221f5
lshoo <lshoo36@gmail.com>
2022-12-21 18:43:30 +0800

    Add reserve tests

--- abi/src/lib.rs
+++ abi/src/lib.rs
@@ -1,6 +1,9 @@
 mod pb;
 
-use chrono::{DateTime, NaiveDateTime, Utc};
+use std::convert::Into;
+use std::fmt;
+
+use chrono::{DateTime, FixedOffset, NaiveDateTime, Utc};
 pub use pb::*;
 use prost_types::Timestamp;
 
@@ -9,4 +12,42 @@ pub fn convert_to_utc_time(ts: Timestamp) -> DateTime<Utc> {
         NaiveDateTime::from_timestamp_opt(ts.seconds, ts.nanos as _).unwrap(),
         Utc,
     )
+}
+
+pub fn convert_to_timestamp(dt: DateTime<Utc>) -> Timestamp {
+    Timestamp {
+        seconds: dt.timestamp(),
+        nanos: dt.timestamp_subsec_nanos() as _,
+    }
+}
+
+impl fmt::Display for ReservationStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ReservationStatus::Pending => write!(f, "pending"),
+            ReservationStatus::Blocked => write!(f, "blocked"),
+            ReservationStatus::Confirmed => write!(f, "confirmed"),
+            ReservationStatus::Unknown => write!(f, "unknown"),
+        }
+    }
+}
+
+impl Reservation {
+    pub fn new_pending(
+        uid: impl Into<String>,
+        rid: impl Into<String>,
+        start: DateTime<FixedOffset>,
+        end: DateTime<FixedOffset>,
+        note: impl Into<String>,
+    ) -> Self {
+        Self {
+            id: "".to_string(),
+            user_id: uid.into(),
+            resource_id: rid.into(),
+            start: Some(convert_to_timestamp(start.with_timezone(&Utc))),
+            end: Some(convert_to_timestamp(end.with_timezone(&Utc))),
+            note: note.into(),
+            status: ReservationStatus::Pending as _,
+        }
+    }
 }
--- reservation/src/manager.rs
+++ reservation/src/manager.rs
@@ -3,7 +3,13 @@ use async_trait::async_trait;
 use crate::{ReservationError, ReservationId, ReservationManager, Rsvp};
 
 use chrono::{DateTime, Utc};
-use sqlx::{postgres::types::PgRange, Row};
+use sqlx::{postgres::types::PgRange, types::Uuid, PgPool, Row};
+
+impl ReservationManager {
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+}
 
 #[async_trait]
 impl Rsvp for ReservationManager {
@@ -20,21 +26,22 @@ impl Rsvp for ReservationManager {
 
         let timespan: PgRange<DateTime<Utc>> = (start..end).into();
 
-        // let status = abi::ReservationStatus::from_i32(rsvp.status).unwrap_or(abi::ReservationStatus::Pending);
+        let status = abi::ReservationStatus::from_i32(rsvp.status)
+            .unwrap_or(abi::ReservationStatus::Pending);
 
-        let id = sqlx::query(
-            "INSERT INTO reservation (user_id, resource_id, timespan, note, status) VALUES ($1, $2, $3, $4,
-            $5) RETURNING id"
+        let id: Uuid = sqlx::query(
+            "INSERT INTO rsvp.reservations (user_id, resource_id, timespan, note, status) VALUES ($1, $2, $3, $4,
+            $5::rsvp.reservation_status) RETURNING id"
         )
             .bind(rsvp.user_id.clone())
             .bind(rsvp.resource_id.clone())
             .bind(timespan)
             .bind(rsvp.note.clone())
-            .bind(rsvp.status)
+            .bind(status.to_string())
             .fetch_one(&self.pool)
             .await?.get(0);
 
-        rsvp.id = id;
+        rsvp.id = id.to_string();
 
         Ok(rsvp)
     }
@@ -67,5 +74,29 @@ impl Rsvp for ReservationManager {
         _query: abi::ReservationQuery,
     ) -> Result<Vec<abi::Reservation>, ReservationError> {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[sqlx_database_tester::test(pool(variable = "migrated_pool", migrations = "../migrations"))]
+    async fn reserve_should_work_for_valid_window() -> Result<(), ReservationError> {
+        let manager = ReservationManager::new(migrated_pool.clone());
+
+        let rsvp = abi::Reservation::new_pending(
+            "james id",
+            "Ocean view room 518",
+            "2022-12-25T15:00:00-0700".parse().unwrap(),
+            "2022-12-30T00:00:00-0700".parse().unwrap(),
+            "I'll arrive at 3pm, Please help to upgrade to execuitive room if possible.",
+        );
+
+        let rsvp = manager.reserve(rsvp).await?;
+        assert!(!rsvp.id.is_empty());
+
+        Ok(())
     }
 }
