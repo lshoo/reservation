51d01d906aa17e5fabd807f04b8e54ec9cad692e
lshoo <lshoo36@gmail.com>
2023-01-05 18:46:27 +0800

    refactor TestConfig to use sqlx-postgres-tester

--- service/src/service.rs
+++ service/src/service.rs
@@ -99,98 +99,39 @@ impl ReservationService for RsvpService {
 #[cfg(test)]
 mod tests {
 
-    use std::{ops::Deref, sync::Arc, thread};
-
     use abi::Reservation;
-    use lazy_static::lazy_static;
-    use sqlx::{types::Uuid, Connection, Executor, PgConnection};
-    use tokio::runtime::Runtime;
+    use sqlx_postgres_tester::TestPg;
+    use std::ops::Deref;
 
     use super::*;
 
     struct TestConfig {
-        config: Arc<Config>,
-    }
-
-    lazy_static! {
-        /// This is an example for using doc comment attributes
-        static ref RT: Runtime = Runtime::new().unwrap();
+        #[allow(dead_code)]
+        tdb: TestPg,
+        config: Config,
     }
 
     impl TestConfig {
         pub fn new() -> Self {
             let mut config = Config::load("../service/fixtures/config.yml").unwrap();
-            // let old_url = config.db.url();
-            let uuid = Uuid::new_v4();
-            let dbname = format!("test_{}", uuid);
-            // let url = config.db.url();
+            let tdb = TestPg::new(config.db.server_url(), "../migrations");
 
-            config.db.dbname = dbname.clone();
+            config.db.dbname = tdb.dbname.clone();
 
-            let server_url = config.db.server_url();
-            let url = config.db.url();
-
-            // create database dbname
-            thread::spawn(move || {
-                RT.block_on(async move {
-                    // use server url to create database
-                    let mut conn = PgConnection::connect(&server_url).await.unwrap();
-                    conn.execute(format!(r#"CREATE DATABASE "{}""#, dbname).as_str())
-                        .await
-                        .expect("Error while create database");
-
-                    let mut conn = PgConnection::connect(&url).await.unwrap();
-                    sqlx::migrate!("../migrations")
-                        .run(&mut conn)
-                        .await
-                        .unwrap();
-                });
-            })
-            .join()
-            .expect("failed to create database");
-
-            Self {
-                config: Arc::new(config),
-            }
+            Self { tdb, config }
         }
     }
 
     impl Deref for TestConfig {
         type Target = Config;
+
         fn deref(&self) -> &Self::Target {
             &self.config
         }
     }
 
-    impl Drop for TestConfig {
-        fn drop(&mut self) {
-            let server_url = self.config.db.server_url();
-
-            let dbname = self.config.db.dbname.clone();
-
-            thread::spawn(move || {
-                RT.block_on(async move {
-                    let mut conn = sqlx::PgConnection::connect(&server_url).await.unwrap();
-                    // terminate existing connections
-                    sqlx::query(&format!(r#"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = '{}'"#, dbname))
-                        .execute(&mut conn)
-                        .await
-                        .expect("Terminate all other connections");
-
-                    conn
-                        .execute(format!(r#"DROP DATABASE "{}""#, dbname).as_str())
-                        .await
-                        .expect("Error while drop database");
-                });
-            })
-            .join()
-            .expect("failed to Drop database");
-        }
-    }
-
     #[tokio::test]
     async fn rpc_reserve_should_work() {
-        // let config = Config::load("../service/fixtures/config.yml").unwrap();
         let config = TestConfig::new();
         let service = RsvpService::from_config(&config).await.unwrap();
         let reservation = Reservation::new_pending(
